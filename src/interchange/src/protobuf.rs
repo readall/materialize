207889a528a621d968d893cf3410e23ac51ac49c
Nikhil Benesch <nikhil.benesch@gmail.com>
2021-11-10 04:01:58 -0500

    interchange: avoid serde-value when decoding protobufs
    When decoding protobufs, it's much clearer and much more efficient to
    decode straight into a `Row`, rather than first decoding into a
    `serde_value::Value` and then converting *that* into a row.
    
    Incidentally this unconvered the issue in #8930:
    default_datum_from_field_nested was incorrectly returning a Float64
    datum for all numeric types.
    
    Fix #8930.

--- src/interchange/src/protobuf.rs
+++ src/interchange/src/protobuf.rs
@@ -11,20 +11,15 @@ use std::collections::HashSet;
 
 use anyhow::{anyhow, bail, Context};
 
-use ordered_float::OrderedFloat;
 use protobuf::descriptor::FileDescriptorSet;
-use protobuf::Message;
-use serde::de::Deserialize;
-use serde_protobuf::de::Deserializer;
+use protobuf::{CodedInputStream, Message as _};
 use serde_protobuf::descriptor::{
     Descriptors, FieldDescriptor, FieldLabel, FieldType, MessageDescriptor,
 };
-use serde_protobuf::value::Value as ProtoValue;
-use serde_value::Value as SerdeValue;
+use serde_protobuf::value::{Field, Message, Value};
 
 use ore::str::StrExt;
-use repr::adt::numeric::Numeric;
-use repr::{ColumnName, ColumnType, Datum, DatumList, Row, ScalarType};
+use repr::{ColumnName, ColumnType, Datum, Row, ScalarType};
 
 /// A decoded description of the schema of a Protobuf message.
 #[derive(Debug)]
@@ -51,7 +46,7 @@ impl DecodedDescriptors {
             // accepted.
             // [0]: https://github.com/dflemstr/serde-protobuf/pull/9
             anyhow!(
-                "Message {} not found in file descriptor set",
+                "protobuf message {} not found in file descriptor set",
                 message_name.quoted()
             )
         })?;
@@ -105,35 +100,20 @@ impl Decoder {
 
     /// Decodes the encoded Protobuf message into a [`Row`].
     pub fn decode(&mut self, bytes: &[u8]) -> Result<Option<Row>, anyhow::Error> {
-        let message = self.descriptors.message_descriptor();
-        let input_stream = protobuf::CodedInputStream::from_bytes(bytes);
-        let mut deserializer =
-            Deserializer::new(&self.descriptors.descriptors, message, input_stream);
-        let deserialized_message =
-            SerdeValue::deserialize(&mut deserializer).context("Deserializing into rust object")?;
-
-        let deserialized_message = match deserialized_message {
-            SerdeValue::Map(deserialized_message) => deserialized_message,
-            _ => bail!("Deserialization failed with an unsupported top level object type"),
-        };
-
-        for (f, (_name, ty)) in message.fields().iter().zip(self.descriptors.columns()) {
-            let key = SerdeValue::String(f.name().to_string());
-            let value = deserialized_message.get(&key);
-            if let Some(value) = value {
-                json_from_serde_value(
-                    &value,
-                    &mut self.packer,
-                    f,
-                    &self.descriptors.descriptors,
-                    ty,
-                )?;
-            } else {
-                self.packer
-                    .push(default_datum_from_field(f, &self.descriptors.descriptors)?);
-            }
-        }
-
+        let message_desc = self.descriptors.message_descriptor();
+        let mut input_stream = CodedInputStream::from_bytes(bytes);
+        let mut message = Message::new(message_desc);
+        message.merge_from(
+            &self.descriptors.descriptors,
+            message_desc,
+            &mut input_stream,
+        )?;
+        pack_message(
+            &mut self.packer,
+            &self.descriptors.descriptors,
+            message_desc,
+            message,
+        )?;
         Ok(Some(self.packer.finish_and_reuse()))
     }
 }
@@ -143,7 +123,8 @@ fn derive_column_type<'a>(
     field: &'a FieldDescriptor,
     descriptors: &'a Descriptors,
 ) -> Result<ColumnType, anyhow::Error> {
-    let scalar_type = match field.field_type(descriptors) {
+    let field_type = field.field_type(descriptors);
+    let scalar_type = match field_type {
         FieldType::Bool => ScalarType::Bool,
         FieldType::Int32 | FieldType::SInt32 | FieldType::SFixed32 => ScalarType::Int32,
         FieldType::Int64 | FieldType::SInt64 | FieldType::SFixed64 => ScalarType::Int64,
@@ -189,276 +170,132 @@ fn derive_column_type<'a>(
             },
         }),
         FieldLabel::Optional => Ok(ColumnType {
-            nullable: field.default_value().is_none(),
+            nullable: matches!(field_type, FieldType::Message(_)),
             scalar_type,
         }),
     }
 }
 
-
-/// Convert an arbitrary [`SerdeValue`] into a [`Datum`], possibly creating a jsonb value
-///
-/// Top-level values are converted to equivalent Datums, but in the case of a nested
-/// type, all numeric types will be converted to f64s (issue #1476)
-fn json_from_serde_value(
-    val: &SerdeValue,
+fn pack_message(
     packer: &mut Row,
-    f: &FieldDescriptor,
     descriptors: &Descriptors,
-    column_type: &ColumnType,
+    message_desc: &MessageDescriptor,
+    message: Message,
 ) -> Result<(), anyhow::Error> {
-    packer.push(match val {
-        SerdeValue::Bool(true) => Datum::True,
-        SerdeValue::Bool(false) => Datum::False,
-        SerdeValue::I8(i) => Datum::Int32(*i as i32),
-        SerdeValue::I16(i) => Datum::Int32(*i as i32),
-        SerdeValue::I32(i) => Datum::Int32(*i),
-        SerdeValue::I64(i) => Datum::Int64(*i),
-        SerdeValue::U8(i) => Datum::Int32(*i as i32),
-        SerdeValue::U16(i) => Datum::Int32(*i as i32),
-        SerdeValue::U32(u) => Datum::from(Numeric::from(*u)),
-        SerdeValue::U64(u) => Datum::from(Numeric::from(*u)),
-        SerdeValue::F32(f) => Datum::Float32((*f).into()),
-        SerdeValue::F64(f) => Datum::Float64((*f).into()),
-        SerdeValue::String(s) => Datum::String(s),
-        SerdeValue::Bytes(b) => Datum::Bytes(b),
-        SerdeValue::Option(s) => {
-            if let Some(s) = s {
-                return json_from_serde_value(&s, packer, f, descriptors, column_type);
-            }
-
-            default_datum_from_field(f, descriptors)?
-        }
-        SerdeValue::Seq(_) | SerdeValue::Map(_) => {
-            return nested_datum_from_serde_value(val, packer, f, descriptors, column_type);
-        }
-        SerdeValue::Char(_) | SerdeValue::Unit | SerdeValue::Newtype(_) => bail!(
-            "Unsupported type for Datum from serde_value::Value: {:?}",
-            val
-        ),
-    });
+    for (field_desc, (_id, field)) in message_desc.fields().iter().zip(message.fields) {
+        pack_field(packer, descriptors, field_desc, field)?;
+    }
     Ok(())
 }
 
-fn default_datum_from_field<'a>(
-    f: &'a FieldDescriptor,
-    descriptors: &'a Descriptors,
-) -> Result<Datum<'a>, anyhow::Error> {
-    if let Some(default) = f.default_value() {
-        return datum_from_serde_proto(default);
-    }
-
-    if f.is_repeated() {
-        return Ok(Datum::List(DatumList::empty()));
-    }
-
-    match f.field_type(descriptors) {
-        FieldType::Bool => Ok(Datum::False),
-        FieldType::Int32 | FieldType::SInt32 | FieldType::SFixed32 => Ok(Datum::Int32(0)),
-        FieldType::Int64 | FieldType::SInt64 | FieldType::SFixed64 => Ok(Datum::Int64(0)),
-        FieldType::Enum(e) => Ok(Datum::String(
-            e.value_by_number(0)
-                .expect("Error while deserializing protobuf: expected enum to have zero variant")
-                .name(),
-        )),
-        FieldType::Float => Ok(Datum::Float32(OrderedFloat::from(0.0))),
-        FieldType::Double => Ok(Datum::Float64(OrderedFloat::from(0.0))),
-        FieldType::UInt32 | FieldType::UInt64 | FieldType::Fixed32 | FieldType::Fixed64 => {
-            Ok(Datum::from(Numeric::from(0)))
-        }
-        FieldType::String => Ok(Datum::String("")),
-        FieldType::Bytes => Ok(Datum::Bytes(&[])),
-        FieldType::Message(_) => Ok(Datum::Null),
-        FieldType::Group => bail!("Unions are currently not supported"),
-        FieldType::UnresolvedMessage(m) => bail!("Unresolved message {} not supported", m),
-        FieldType::UnresolvedEnum(e) => bail!("Unresolved enum {} not supported", e),
+fn pack_field(
+    packer: &mut Row,
+    descriptors: &Descriptors,
+    field_desc: &FieldDescriptor,
+    field: Field,
+) -> Result<(), anyhow::Error> {
+    match field {
+        Field::Singular(None) => pack_default_value(packer, descriptors, field_desc),
+        Field::Singular(Some(value)) => pack_value(packer, descriptors, field_desc, value),
+        Field::Repeated(values) => packer.push_list_with(|packer| {
+            for value in values {
+                pack_value(packer, descriptors, field_desc, value)?;
+            }
+            Ok::<_, anyhow::Error>(())
+        }),
     }
 }
 
-fn nested_datum_from_serde_value(
-    val: &SerdeValue,
+fn pack_value(
     packer: &mut Row,
-    f: &FieldDescriptor,
     descriptors: &Descriptors,
-    column_type: &ColumnType,
+    field_desc: &FieldDescriptor,
+    value: Value,
 ) -> Result<(), anyhow::Error> {
-    packer.push(match val {
-        SerdeValue::Bool(true) => Datum::True,
-        SerdeValue::Bool(false) => Datum::False,
-        SerdeValue::I8(i) => Datum::Int32(*i as i32),
-        SerdeValue::I16(i) => Datum::Int32(*i as i32),
-        SerdeValue::I32(i) => Datum::Int32(*i),
-        SerdeValue::I64(i) => Datum::Int64(*i),
-        SerdeValue::U8(i) => Datum::Int32(*i as i32),
-        SerdeValue::U16(i) => Datum::Int32(*i as i32),
-        SerdeValue::U32(u) => Datum::from(Numeric::from(*u)),
-        SerdeValue::U64(u) => Datum::from(Numeric::from(*u)),
-        SerdeValue::F32(f) => Datum::Float32((*f).into()),
-        SerdeValue::F64(f) => Datum::Float64((*f).into()),
-        SerdeValue::String(s) => Datum::String(s),
-        SerdeValue::Bytes(_) => {
-            bail!("We don't currently support arrays or nested messages with bytes")
-        }
-        SerdeValue::Seq(s) => {
-            let inner_column_type = if let ColumnType {
-                scalar_type: ScalarType::List { element_type, .. },
-                ..
-            } = column_type
-            {
-                ColumnType {
-                    scalar_type: *element_type.clone(),
-                    nullable: true,
-                }
-            } else {
-                bail!("sequence must be of type list, found {:?}", column_type);
+    match value {
+        Value::Bool(false) => packer.push(Datum::False),
+        Value::Bool(true) => packer.push(Datum::True),
+        Value::I32(i) => packer.push(Datum::Int32(i)),
+        Value::I64(i) => packer.push(Datum::Int64(i)),
+        Value::F32(f) => packer.push(Datum::Float32(f.into())),
+        Value::F64(f) => packer.push(Datum::Float64(f.into())),
+        Value::String(s) => packer.push(Datum::String(&s)),
+        Value::Bytes(b) => packer.push(Datum::Bytes(&b)),
+        Value::Enum(e) => {
+            let enum_desc = match field_desc.field_type(descriptors) {
+                FieldType::Enum(enum_desc) => enum_desc,
+                ty => bail!(
+                    "internal error: protobuf enum value has unexpected type {:?}",
+                    ty
+                ),
             };
-            return packer.push_list_with(|packer| {
-                for value in s {
-                    nested_datum_from_serde_value(
-                        &value,
-                        packer,
-                        f,
-                        descriptors,
-                        &inner_column_type,
-                    )?;
+            match enum_desc.value_by_number(e) {
+                None => {
+                    bail!(
+                        "error decoding protobuf: enum value {} is missing while decoding field {}",
+                        e,
+                        field_desc.name()
+                    );
                 }
-                Ok(())
-            });
-        }
-        SerdeValue::Option(v) => {
-            if let Some(v) = v {
-                return nested_datum_from_serde_value(&v, packer, f, descriptors, column_type);
-            }
-
-            default_datum_from_field_nested(f, descriptors)?
-        }
-        SerdeValue::Map(m) => {
-            let nested_message_descriptor = f.field_type(descriptors);
-            let pack_map = |packer: &mut Row,
-                            k: &SerdeValue,
-                            v: &SerdeValue,
-                            typ: &ColumnType,
-                            push_key: bool| {
-                match k {
-                    SerdeValue::String(s) => {
-                        if push_key {
-                            packer.push(Datum::String(s.as_str()));
-                        }
-
-                        let nested_message_descriptor = match nested_message_descriptor {
-                            FieldType::Message(m) => m,
-                            _ => bail!("Nested message is the wrong type"),
-                        };
-
-                        nested_datum_from_serde_value(
-                            &v,
-                            packer,
-                            nested_message_descriptor
-                                .field_by_name(s)
-                                .expect("nested message to exist"),
-                            descriptors,
-                            typ,
-                        )?;
-                    }
-                    _ => bail!("Unrecognized value while trying to parse a nested message"),
-                }
-                Ok(())
-            };
-
-            match &column_type.scalar_type {
-                ScalarType::Map { value_type, .. } => {
-                    let mut kvs = m.iter().collect::<Vec<_>>();
-                    kvs.sort_by(|(k1, _v1), (k2, _v2)| k1.cmp(k2));
-
-                    return packer.push_dict_with(|packer| {
-                        let typ = ColumnType {
-                            scalar_type: *value_type.clone(),
-                            nullable: true,
-                        };
-                        for (k, v) in kvs {
-                            pack_map(packer, k, v, &typ, true)?;
-                        }
-                        Ok(())
-                    });
-                }
-                ScalarType::Record { fields, .. } => {
-                    return packer.push_list_with(|packer| {
-                        for (n, typ) in fields {
-                            let (k, v) = m
-                                .get_key_value(&SerdeValue::String(n.to_string()))
-                                .expect("key value pair to exist");
-                            pack_map(packer, k, v, &typ, false)?;
-                        }
-                        Ok(())
-                    });
-                }
-                _ => bail!("Unsupported scalar type for map"),
+                Some(ev) => packer.push(Datum::String(ev.name())),
             }
         }
-        _ => bail!("Unsupported types from serde_value"),
-    });
+        Value::Message(m) => {
+            let message_desc = match field_desc.field_type(descriptors) {
+                FieldType::Message(message_desc) => message_desc,
+                ty => bail!(
+                    "internal error: protobuf message value has unexpected type {:?}",
+                    ty
+                ),
+            };
+            packer.push_list_with(|packer| pack_message(packer, descriptors, message_desc, m))?;
+        }
+        Value::U32(_) | Value::U64(_) => {
+            bail!(
+                "internal error: unexpected value while decoding protobuf message: {:?}",
+                value
+            );
+        }
+    };
     Ok(())
 }
 
-fn default_datum_from_field_nested<'a>(
-    f: &'a FieldDescriptor,
-    descriptors: &'a Descriptors,
-) -> Result<Datum<'a>, anyhow::Error> {
-    if let Some(default) = f.default_value() {
-        return datum_from_serde_proto_nested(default);
+fn pack_default_value(
+    packer: &mut Row,
+    descriptors: &Descriptors,
+    field_desc: &FieldDescriptor,
+) -> Result<(), anyhow::Error> {
+    if let Some(default) = field_desc.default_value() {
+        return pack_value(packer, descriptors, field_desc, default.clone());
     }
-
-    if f.is_repeated() {
-        return Ok(Datum::List(DatumList::empty()));
+    match field_desc.field_type(descriptors) {
+        FieldType::Bool => packer.push(Datum::False),
+        FieldType::Int32 | FieldType::SInt32 | FieldType::SFixed32 => packer.push(Datum::Int32(0)),
+        FieldType::Int64 | FieldType::SInt64 | FieldType::SFixed64 => packer.push(Datum::Int64(0)),
+        FieldType::Float => packer.push(Datum::Float32(0.0.into())),
+        FieldType::Double => packer.push(Datum::Float64(0.0.into())),
+        FieldType::String => packer.push(Datum::String("")),
+        FieldType::Bytes => packer.push(Datum::Bytes(&[])),
+        FieldType::Message(_) => packer.push(Datum::Null),
+        FieldType::Enum(e) => match e.value_by_number(0) {
+            None => bail!(
+                "error decoding protobuf: enum value 0 is missing while decoding field {}",
+                field_desc.name()
+            ),
+            Some(ev) => packer.push(Datum::String(ev.name())),
+        },
+        ty @ FieldType::UInt32
+        | ty @ FieldType::UInt64
+        | ty @ FieldType::Fixed32
+        | ty @ FieldType::Fixed64
+        | ty @ FieldType::Group
+        | ty @ FieldType::UnresolvedMessage(_)
+        | ty @ FieldType::UnresolvedEnum(_) => {
+            bail!(
+                "internal error: unexpected type while decoding protobuf message: {:?}",
+                ty
+            );
+        }
     }
-
-    match f.field_type(descriptors) {
-        FieldType::Bool => Ok(Datum::False),
-        FieldType::Int32
-        | FieldType::SInt32
-        | FieldType::SFixed32
-        | FieldType::Int64
-        | FieldType::SInt64
-        | FieldType::SFixed64
-        | FieldType::UInt32
-        | FieldType::UInt64
-        | FieldType::Fixed32
-        | FieldType::Fixed64
-        | FieldType::Float
-        | FieldType::Double => Ok(Datum::Float64(OrderedFloat::from(0.0))),
-        FieldType::Enum(e) => Ok(Datum::String(
-            e.value_by_number(0)
-                .expect("Error while deserializing protobuf: expected enum to have zero variant")
-                .name(),
-        )),
-        FieldType::String => Ok(Datum::String("")),
-        FieldType::Message(_) => Ok(Datum::Null),
-        FieldType::Bytes => bail!("Nested bytes are not supported"),
-        FieldType::Group => bail!("Unions are currently not supported"),
-        FieldType::UnresolvedMessage(m) => bail!("Unresolved message {} not supported", m),
-        FieldType::UnresolvedEnum(e) => bail!("Unresolved enum {} not supported", e),
-    }
-}
-
-fn datum_from_serde_proto<'a>(val: &'a ProtoValue) -> Result<Datum<'a>, anyhow::Error> {
-    match val {
-        ProtoValue::Bool(true) => Ok(Datum::True),
-        ProtoValue::Bool(false) => Ok(Datum::False),
-        ProtoValue::I32(i) => Ok(Datum::Int32(*i)),
-        ProtoValue::I64(i) => Ok(Datum::Int64(*i)),
-        ProtoValue::U32(u) => Ok(Datum::from(Numeric::from(*u))),
-        ProtoValue::U64(u) => Ok(Datum::from(Numeric::from(*u))),
-        ProtoValue::F32(f) => Ok(Datum::Float32((*f).into())),
-        ProtoValue::F64(f) => Ok(Datum::Float64((*f).into())),
-        ProtoValue::String(s) => Ok(Datum::String(s)),
-        ProtoValue::Bytes(b) => Ok(Datum::Bytes(b)),
-        _ => bail!("Unsupported type for Datum from serde_protobuf::Value"),
-    }
-}
-
-fn datum_from_serde_proto_nested<'a>(val: &'a ProtoValue) -> Result<Datum<'a>, anyhow::Error> {
-    if let ProtoValue::Bytes(_) = val {
-        bail!("Nested bytes are not supported");
-    }
-    datum_from_serde_proto(val)
+    Ok(())
 }
